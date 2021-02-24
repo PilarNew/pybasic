379670611568b9a4ca3f4a8531b67963ce343969
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-24 17:17:58 -0300

    Se trabaja con variables básicas

---
+++ 02-variables-y-tipos/variables.py
@@ -0,0 +1,20 @@
+"""
+Variable -->  básicamente un contenedor de información que guardará dentro de sí un data. 
+Se pueden crear muchas variables. Cada una puede tener un valor distinto
+
+"""
+texto = "Máster en Python"
+texto2 = "practicando python"
+numero = 45
+decimal = 6.7
+print(texto)
+print(texto2)
+print(numero)
+print(decimal)
+
+print("--------------------------")
+
+numero = 77
+decimal=8.9
+print(numero)
+print(decimal)
