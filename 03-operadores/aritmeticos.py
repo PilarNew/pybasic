b41a7247570f7b52c40309c363baa7eb8a596807
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-24 20:31:41 -0300

    04-Operadores aritméticos o matemáticos

---
+++ 03-operadores/aritmeticos.py
@@ -0,0 +1,15 @@
+# Operadores Aritméticos
+numero1 = 78
+numero2 = 2 # Operador asignación = 
+
+resta = numero1 - numero2
+multiplicacion = numero1*numero2
+division=numero1/numero2
+
+resto = numero1 % numero2
+print("**************CALCULADORA*****************")
+print(f"La suma es: {numero1+numero2}")
+print(f"La resta es {resta}")
+print("La multiplicación es:", multiplicacion)
+print("La división es:", division)
+print("El resto es:", resto)
