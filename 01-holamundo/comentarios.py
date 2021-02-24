01bdeae699aecdee5304201e4a1417e514bbcaba
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-24 17:04:59 -0300

    Se crea archivo para comentarios simples y multilínea

---
+++ 01-holamundo/comentarios.py
@@ -0,0 +1,12 @@
+# Esto me mostrará el texto Hola Mundo por la consola
+print("Hola")
+# Estas dos instrucciones muestran código por pantalla
+print("Mundo")
+print("!!!")
+
+# Comentario no es considerado por el intérprete
+"""
+print("Mundo")
+print("!!!")
+
+"""
