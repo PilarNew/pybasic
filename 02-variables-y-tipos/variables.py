e6311b2819f8de96e9cdb9c72e886afc8de740b8
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-24 17:21:18 -0300

    Se agregan comentarios 02

--- 02-variables-y-tipos/variables.py
+++ 02-variables-y-tipos/variables.py
@@ -3,10 +3,13 @@ Variable -->  básicamente un contenedor de información que guardará dentro de
 Se pueden crear muchas variables. Cada una puede tener un valor distinto
 
 """
+
+# Creación de variables y asignarles un valor
 texto = "Máster en Python"
 texto2 = "practicando python"
 numero = 45
 decimal = 6.7
+# Mostrar valor de las variables
 print(texto)
 print(texto2)
 print(numero)
@@ -14,7 +17,10 @@ print(decimal)
 
 print("--------------------------")
 
+# Reasignación de valores a las variables
 numero = 77
 decimal=8.9
 print(numero)
 print(decimal)
+
+print("--------------------------")
