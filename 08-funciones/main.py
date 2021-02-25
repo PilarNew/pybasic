a48388c5d9c54ffae885dee8aeb532af7cf43efa
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 19:08:14 -0300

    42-Return-Calculadora[C

---
+++ 08-funciones/main.py
@@ -0,0 +1,117 @@
+# """
+# FUNCIONES:
+# Una fn es un cjto de instrucciones agrupadas bajo un nombre concreto que pueden reutilizarse invocando a la fn tantas veces como ea necesario 
+
+# def nombreDeMiFuncion(parámetros):
+#     # BLOQUE / CJTO DE INSTRUCCIONES
+
+# nombreDeMiFuncion(mi_parametro)
+
+# """
+
+# # Ejemplo 1
+# print("######### EJEMPLO 1 ########")
+
+# # Definición de función
+# def muestraNombre():
+#     print("Pilar")
+#     print("Pedro")
+#     print("Pancho")
+#     print("Petronia")
+#     print("Paco")
+#     print("Paz")
+#     print("\n")
+
+# # Invocación de la función
+# muestraNombre()
+# muestraNombre()
+# muestraNombre()
+
+# # Ejemplo 2 Parámetros
+# print("######### EJEMPLO 2 ########")
+
+# nombre = input("Ingresa tu nombre: ")
+# edad = int(input("Ingresa tu edad: "))
+
+# def muestraTuNombre(data,edad):
+#     print(f"Hola {data}")
+#     if edad > 18:
+#         print(f"{data} eres mayor de edad")
+
+# muestraTuNombre(nombre,edad)
+
+# # Ejemplo 3
+# print("######### EJEMPLO 3 ########")
+
+# def tabla(numero):
+#     print(f"Tabla de multiplicar de: {numero}")
+
+#     for contador in range(11):
+#         operacion = numero*contador
+#         print(f"{numero}*{contador}= {operacion}")
+#     print("\n")
+
+# tabla(3)
+# tabla(7)
+
+# # Ejemplo 3.1
+
+# for numero_tabla in range(1,11):
+#     tabla(numero_tabla)
+
+
+# # Ejemplo 4 -  Parámetros opcionales
+# print("######### EJEMPLO 4 ########")
+
+
+# def getEmpleado(nombre,run=None):
+#     print("EMPLEADO")
+#     print(f"Nombre: {nombre}")
+#     if run != None:
+#         print(f"RUN: {run}")
+
+# getEmpleado("Pilar","333333")
+
+
+# # Ejemplo 5 -  Return - Parámetros opcionales
+# print("######### EJEMPLO 5 ########")
+
+# def saludame(nombre):
+#     saludo = f"Hola saludos {nombre}"
+#     return saludo
+
+# print(saludame("Nana"))
+
+
+# Ejemplo 6
+print("\n######### EJEMPLO 6 ########")
+
+def calculadora(numero1,numero2,basicas = False):
+    suma = numero1 + numero2
+    resta = numero1 - numero2
+    division = numero1 / numero2
+    multiplicacion = numero1 * numero2
+
+    cadena ="" 
+
+    if basicas != False:
+        cadena += "Suma: " + str(suma)
+        cadena += "\n"
+        cadena += "Resta: " + str(resta)
+        cadena += "\n"
+    else:
+        cadena += "Suma: " + str(suma)
+        cadena += "\n"
+        cadena += "Resta: " + str(resta)
+        cadena += "\n"
+        cadena += "Multiplicación: " + str(multiplicacion)
+        cadena += "\n"
+        cadena += "División: " + str(division)
+        cadena += "\n"
+
+    return cadena
+   
+
+    
+    
+print(calculadora(5,5))
