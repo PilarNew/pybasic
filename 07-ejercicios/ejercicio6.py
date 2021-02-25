d6c38476c60c15f590f7d910673e4d370ca95b3c
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 17:31:05 -0300

    08-Ejercicio 6 - Se muestran tablas de multiplicar hasta el la del 10 dentro de un rango

--- 07-ejercicios/ejercicio6.py
+++ 07-ejercicios/ejercicio6.py
@@ -1,4 +1,14 @@
 """
 Ejercicio 6
-
+- Mostrar todas las tablas de multiplicar del 1 al 10.
+- Mostrando el título de la tabla y luego
 """
+for titulo in range(1,11):
+    print("################################")
+    print(f"######TABLA DEL {titulo} #######")
+    print("################################")
+
+    for numero in range(1,11):
+        print(f"{numero} * {titulo} = {numero*titulo}")
+    
+    print("\n")
