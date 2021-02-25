7e99f70de70f3f120bf2ec0455021305756f3eca
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-24 22:46:30 -0300

    05-Entrada y salida de datos

---
+++ 04-entrada-salida/main.py
@@ -0,0 +1,5 @@
+# Entrada
+nombre = input("¿Cuál es tu nombre?:")
+edad = input("¿Cuál es tu edad?")
+# Salida
+print(f"Me alegro de conocerte, bienvenido {nombre}, veo que tienes {int(edad) + 2} años!!")
