779dfa8d2faf63c4d53637fe24aa4f40412ca3eb
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 15:46:02 -0300

    07-bucle while con table de multiplicar

--- 06-bucles/bucle-while.py
+++ 06-bucles/bucle-while.py
@@ -19,3 +19,18 @@ while contador <= 10:
     muestrame = muestrame + "," + str(contador)
     contador +=1
 print(muestrame)
+
+# Ejemplo
+
+print("################ EJEMPLO ################")
+numero_usuario = int(input("¿De qué número quieres la tabla?: "))
+if numero_usuario < 1:
+    numero_usuario = 1
+print(f"####### TABLA DEL {numero_usuario} ##########")
+
+contador = 1
+while contador <= 10:
+    print(f"{numero_usuario} x {contador} = {numero_usuario * contador}")
+    contador +=1
+else:
+    print("Tabla terminada")
