3661dd0b31995848795d94aeb7f765749394e3ee
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 18:01:53 -0300

    08-Ejercicio 10 - Se asigna la categoría de aprobado o reprobado de acuerdo a nota asignada

--- 07-ejercicios/ejercicio10.py
+++ 07-ejercicios/ejercicio10.py
@@ -1,4 +1,24 @@
 """
 Ejercicio 10
 
+- El programa tiene que pedir la nota de 15 alumnos y sacar por pantalla cuántos han aprobado y cuántos has suspendido
+
 """
+
+contador = 1
+aprobados = 0
+reprobados = 0
+
+numero_alumnos = int(input("¿Cuántos alumnos tienes?: "))
+
+while contador <= numero_alumnos:
+    nota = int(input(f'¿Qué nota quieres ponerle al \"alumno {contador}\" ?: '))
+
+    if nota >= 4:
+        aprobados +=1
+    else:
+        reprobados +=1
+
+    contador += 1
+
+print(f"Tienes {aprobados} alumnos aprobados y {reprobados} alumnos reprobados")
