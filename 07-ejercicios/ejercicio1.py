6c63ee50676c912e6e5511ea4a0d9cf0c72f5906
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 15:58:37 -0300

    08-Ejercicio 1

---
+++ 07-ejercicios/ejercicio1.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 1
+- Crear variable "pais" y "continente"
+- Mostrar su valor por pantalla (imprimir)
+- Poner un comentario diciendo el tipo de dato
+"""
+
+pais ="Chile" # Tipo de dato str
+continente = "América" # Tipo de dato str
+year = 2021 # Tipo de dato int
+print(f"{pais} - {continente}, {str(year)}")
