eeb09690d70e573f19f482a931567592e9679131
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 17:38:49 -0300

    08-Ejercicio 7 - Se muestran números pares e impares entre dos números señalados por el usuario

--- 07-ejercicios/ejercicio7.py
+++ 07-ejercicios/ejercicio7.py
@@ -1,4 +1,18 @@
 """
 Ejercicio 7
 
+- Hacer un programa que muestre todos los números impares entre dos números que decida el usuario
 """
+
+numero_uno = int(input("Introduce un número: "))
+numero_dos = int(input("Introduce otro número: "))
+
+if numero_uno < numero_dos:
+
+    for x in range(numero_uno,numero_dos + 1):
+        if x % 2 == 0:
+            print(f"{x} es PAR")
+        else:
+            print(f"{x} es IMPAR")
+else:
+    print("El primer número debe ser menor al segundo")
