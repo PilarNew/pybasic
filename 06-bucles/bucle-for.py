4d549cfe3fa94bae6355d77c52d66e2c5fb5e243
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 15:17:51 -0300

    07-bucle for con break

--- 06-bucles/bucle-for.py
+++ 06-bucles/bucle-for.py
@@ -12,3 +12,20 @@ for contador in range(0,5):
     resultado += contador
 # Por la identación sólo se muestra el último valor de la variable resultado
 print(f"El resultado es: {resultado}")
+
+# Ejemplo tablas de multiplicar
+print("\n##################### EJEMPLO ######################")
+
+numero_usuario = int(input("¿De qué número quieres la tabla?: "))
+
+if numero_usuario < 1:
+   numero_usuario = 1
+print(f"\n############### Tabla de multiplicar del número {numero_usuario}")
+
+for numero_tabla in range(1,11):
+    if numero_usuario == 45:
+        print("No se puede mostrar. El número ingresado está prohibido!!")
+        break
+    print(f"{numero_usuario} x {numero_tabla} = {numero_usuario*numero_tabla}")
+else:
+    print("Tabla finzalizada")
