f962b30d36179490dac11cdad907521f64aa2336
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 16:10:30 -0300

    08-Ejercicio 2 - números pares

---
+++ 07-ejercicios/ejercicio2.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 2
+
+- Escribir un script que muestre por pantalla todos los números del 1 al 120
+
+"""
+contador  =  1
+
+for contador in range(1,121):
+    if contador % 2 == 0:  
+      print(contador)
