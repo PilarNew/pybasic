c0702bd97c6a0d38967bdc89d1a5539814beb52a
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-24 21:00:58 -0300

    04-Operadores de asignacióm +=,  -=

---
+++ asignacion.py
@@ -0,0 +1,12 @@
+# Operadores de asignación
+edad = 55
+print(edad)
+
+# Expresiones equivalentes; se puede utilizar con multilpicación, división, etc.
+edad+=5
+edad = edad + 6
+
+
+edad -= 5
+
+print(edad)
