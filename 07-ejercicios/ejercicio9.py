2d318c180201818693cf39a37a72ff725866f458
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 17:50:36 -0300

    08-Ejercicio 9 - Se introducen números hasta adivinar el número secreto

--- 07-ejercicios/ejercicio9.py
+++ 07-ejercicios/ejercicio9.py
@@ -1,4 +1,15 @@
 """
 Ejercicio 9
 
+- Hacer un programa que pida números al usuario indefinidamente hasta meter el número 111
+
 """
+contador = 1
+
+while contador < 100:
+    numero = int(input("Introduce un número: "))
+
+    if numero == 111:
+        break
+    else:
+        print(f"Has introducido el {numero}")
