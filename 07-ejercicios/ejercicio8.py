06dbb3b5c27ad7a7fc1c83551c51cc7d3019db8e
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 17:43:19 -0300

    08-Ejercicio 8 - Se calcula un porcentaje de un número; ambos valores ingresados por el usuario en consola

--- 07-ejercicios/ejercicio8.py
+++ 07-ejercicios/ejercicio8.py
@@ -1,4 +1,11 @@
 """
 Ejercicio 8
 
+- Calcular el x% de x número
 """
+
+numero = int(input("Introduce el número: "))
+porcentaje = int(input(f"¿Qué porcentaje quieres sacar de {numero}?: "))
+
+calculo = numero*porcentaje/100
+print(f"El {porcentaje}% de {numero} es {calculo}")
