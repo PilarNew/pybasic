bc6e0680b41a01d0b9506b248087b1e071f9f26d
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 18:21:23 -0300

    39-creación de funciones

---
+++ 07-ejercicios/08-funciones/main.py
@@ -0,0 +1,28 @@
+"""
+FUNCIONES:
+Una fn es un cjto de instrucciones agrupadas bajo un nombre concreto que pueden reutilizarse invocando a la fn tantas veces como ea necesario 
+
+def nombreDeMiFuncion(parámetros):
+    # BLOQUE / CJTO DE INSTRUCCIONES
+
+nombreDeMiFuncion(mi_parametro)
+
+"""
+
+# Ejemplo 1
+print("######### EJEMPLO 1 ########")
+
+# Definición de función
+def muestraNombre():
+    print("Pilar")
+    print("Pedro")
+    print("Pancho")
+    print("Petronia")
+    print("Paco")
+    print("Paz")
+    print("\n")
+
+# Invocación de la función
+muestraNombre()
+muestraNombre()
+muestraNombre()
