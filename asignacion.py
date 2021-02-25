34abc26c5789dc8f39dfe92d4667387c613f0b89
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-24 21:36:10 -0300

    04-Incremento y decremento

--- asignacion.py
+++ asignacion.py
@@ -10,3 +10,21 @@ edad = edad + 6
 edad -= 5
 
 print(edad)
+
+# Operadores de incremento y decremento
+year = 2021
+print(year)
+# Incremento
+# year = year + 1
+year +=1
+print(year)
+# Decremento
+#year = year - 1
+year -= 1
+print(year)
+# Pre incremento
+year = 1 + year
+print(year)
+# Pre decremento
+year = 1 - year
+print(year)
