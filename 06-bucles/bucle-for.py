60e4d43625ea40841c1ada3840d253c2324e9322
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-25 14:46:18 -0300

    07-se trabaja con bucle for

---
+++ 06-bucles/bucle-for.py
@@ -0,0 +1,14 @@
+"""
+# FOR
+
+for variable in elemento_iterable (lista, rango, etc)
+    BLOQUE DE INSTRUCCIONES
+"""
+contador = 0
+resultado = 0
+
+for contador in range(0,5):
+    print(f"voy en el {str(contador)}")
+    resultado = resultado + contador
+# Por la identación sólo se muestra el último valor de la variable resultado
+print(f"El resultado es: {resultado}")
