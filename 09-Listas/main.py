0f4d543411062c5694e3e0811cdee63a5a4df7b8
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-26 17:53:04 -0300

    48-Crear listas

---
+++ 09-Listas/main.py
@@ -0,0 +1,24 @@
+"""
+LISTAS (arrays)
+
+Son colecciones o conjuntos de datos/valores, bajo un único nombre.
+Para acceder a esos valores podemos usar un índice numérico.
+
+"""
+
+pelicula ="Batman"
+
+peliculas = ["Batman","Spiderman", "El Señor de los anillos"]
+
+# Tupla -> no se puede modificar
+cantantes = list(('2pac','Drake', 'Jennifer López'))
+
+# Rango de números
+years = list(range(2020,2050))
+variada = ["Victor", 10,4.4, True, "Texto"]
+
+print(peliculas)
+print(cantantes)
+print(years)
+print(variada)
+
