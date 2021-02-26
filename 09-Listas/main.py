645b72ff9356c523637f04c0cffff86bb7397fcd
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-26 18:12:28 -0300

    49-índices de listas

--- 09-Listas/main.py
+++ 09-Listas/main.py
@@ -8,17 +8,26 @@ Para acceder a esos valores podemos usar un índice numérico.
 
 pelicula ="Batman"
 
-peliculas = ["Batman","Spiderman", "El Señor de los anillos"]
+peliculas = ["Batman","Spiderman", "El Señor de los anillos","Hello"]
 
 # Tupla -> no se puede modificar
-cantantes = list(('2pac','Drake', 'Jennifer López'))
+cantantes = list(('2pac','Drake', 'Jennifer López',"Phill"))
 
 # Rango de números
 years = list(range(2020,2050))
 variada = ["Victor", 10,4.4, True, "Texto"]
 
-print(peliculas)
-print(cantantes)
-print(years)
-print(variada)
+# print(peliculas)
+# print(cantantes)
+# print(years)
+# print(variada)
 
+# Índices positivos de izquierda 0
+# De derecha a izquierda se comienza con -1
+# Se reemplaza el valor del índice 1
+peliculas[1] ="Gran Torino"
+print(peliculas)
+print(peliculas[0])
+print(peliculas[-1])
+print(peliculas[1:3])
+print(peliculas[1:])
