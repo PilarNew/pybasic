523b73dd30888e009495e7c3a160a882fdb94252
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-26 22:55:45 -0300

    56-Diccionarios dentro de listas

--- 10-set-diccionarios/diccionarios.py
+++ 10-set-diccionarios/diccionarios.py
@@ -14,3 +14,34 @@ persona ={
 print(persona)
 print(type(persona))
 print(persona["apellido"])
+
+
+# Lista con diccinarios (Objeto JSON)
+
+contactos = [
+    {
+        "nombre":"Antonio",
+        "email":"antonio@gmail.com"
+    },
+    {    
+        "nombre":"Juan",
+        "email":"juan@gmail.com"
+    },
+    {
+        "nombre":"Mario",
+        "email":"mario@gmail.com"
+    },
+    {   "nombre":"Claudio",
+        "email":"claudio@gmail.com"
+    }
+]
+contactos[0]["nombre"]="Toñito"
+print(contactos[0]["nombre"])
+
+print("\nLista de Contactos")
+
+print("--------------------------------------")
+for contacto in contactos:
+    print(f"Nombre del contacto: {contacto['nombre']}")
+    print(f"Email del contacto: {contacto['email']}")
+    print("--------------------------------------")
