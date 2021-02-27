3a68c23db6bb621269043deacad8c1e3f4ac53f0
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-27 18:01:05 -0300

    67-paquetes

---
+++ 13-paquetes/main.py
@@ -0,0 +1,8 @@
+
+print("PROBANDO PAQUETES: ")
+
+# from mipaquete import pruebas
+# from mipaquete import herramientas
+from mipaquete import pruebas, herramientas
+pruebas.probando()
+herramientas.nombreCompleto("Pilar", "González")
