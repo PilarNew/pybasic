d67475a6eba5b9dc8d415c11e7fd5146080aa43d
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-26 22:39:58 -0300

    54-Diccionarios

---
+++ 10-set-diccionarios/diccionarios.py
@@ -0,0 +1,16 @@
+"""
+DICCIONARIO
+Un tipo de datos que almacena un conjunto de datos. En formato clave > valor
+Es parecido a un array asociativo o un objeto JSON
+Índice alfanumérico
+"""
+
+persona ={
+"nombre": "Pili",
+"apellido": "González",
+"edad": "?"
+}
+
+print(persona)
+print(type(persona))
+print(persona["apellido"])
