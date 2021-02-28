981b7cf4bec12ff72458f14e5452cfcc4128785f
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-28 20:16:28 -0300

    76- Ejercio manejo de errores try/except

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -1,3 +1,4 @@
+"""
 # Capturar excepciones y manejar errores en código susceptible a fallos/errores
 
 try:
@@ -14,3 +15,28 @@ finally:
     print("Fin de la iteración!!!")
 
 # Si no se especificaba un nombre se generaba un error interno, pero con try y except se puede hacer un manejo de errores
+"""
+
+# Búsqueda en la lista
+numeros = [100,20,15,4000,56,88]
+try:
+    buscado = int(input("Ingresa número: "))
+
+    comprobar = isinstance(buscado,int)
+
+    while not comprobar or buscado <= 0:
+        buscado = int(input("Ingresa número: "))
+    else:
+        print(f"Has introducido el {buscado}")
+
+    print(f"########## Buscar en la lista {buscado} ###########")
+
+
+    search = numeros.index(buscado)
+    print(f"El número buscado existe en la lista es el ínice {search}; {numeros[search]}")
+except:
+    print("El número no está en la lista")
+
+
+
+
