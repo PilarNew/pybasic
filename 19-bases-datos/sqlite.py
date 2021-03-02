c07b41bcf63dd4052be3393d7c131522ff46b2b1
NayarettGonzalez <nagonzalezv@uc.cl>
2021-03-02 18:18:01 -0300

    92-Insertando datos a través de sqlite3

--- 19-bases-datos/sqlite.py
+++ 19-bases-datos/sqlite.py
@@ -15,5 +15,22 @@ cursor.execute("CREATE TABLE IF NOT EXISTS productos("+
 
 # Guardar cambios
 conn.commit()
+
+# Insertar datos
+cursor.execute("INSERT INTO productos VALUES (null, 'Primer producto', 'Descripción de primer producto', 1000)")
+conn.commit()
+
+# Listar datos
+cursor.execute("SELECT * FROM productos;")
+productos = cursor.fetchall()
+for producto in productos:
+    print("Nombre:", producto[1])
+    print("Descripción:", producto[2])
+    print("Precio: $",producto[3])
+    print("\n")
+cursor.execute("SELECT titulo FROM productos;")
+producto = cursor.fetchone() # Saca el primer producto
+conn.commit()
+
 # Cerrar conexión
 conn.close()
