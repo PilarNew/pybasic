c9cf282224b77f02d070761c1e53bd2536456217
NayarettGonzalez <nagonzalezv@uc.cl>
2021-03-03 20:12:26 -0300

    99-Insertar registros

--- 19-bases-datos/main.py
+++ 19-bases-datos/main.py
@@ -24,19 +24,31 @@ for bd in cursor:
     print(bd)
 """
 
-# Crear tablas
-cursor.execute("""
-CREATE TABLE vehiculos(
-    id int(10) auto_increment not null,
-    marca varchar(40) not null,
-    modelo varchar(40) not null,
-    precio float(10,2) not null,
-    CONSTRAINT pk_vehiculo PRIMARY KEY(id)
-)
-""")
+# # Crear tablas
+# cursor.execute("""
+# CREATE TABLE vehiculos(
+#     id int(10) auto_increment not null,
+#     marca varchar(40) not null,
+#     modelo varchar(40) not null,
+#     precio float(10,2) not null,
+#     CONSTRAINT pk_vehiculo PRIMARY KEY(id)
+# )
+# """)
 
-# Para saber en el códgo si existe la tabla recién creada para no ir al administrador
-cursor.execute("SHOW DATABASES")
-# Muestra todas las bases de datos existentes
-for table in cursor:
-    print(table)
+# # Para saber en el códgo si existe la tabla recién creada para no ir al administrador
+# cursor.execute("SHOW TABLES")
+# # Muestra todas las bases de datos existentes
+# for table in cursor:
+#     print(table)
+
+# cursor.execute("INSERT INTO vehiculos VALUES(null, 'Opel', 'Astra', 18500)")
+coches = [
+    ('Seat', 'Ibiza',5000),
+    ('Renault', 'Clio',15000),
+    ('Citroen', 'Saxo',2000),
+    ('Mercedes', 'Clase C',35000)
+]
+
+cursor.executemany("INSERT INTO vehiculos VALUES (null, %s, %s, %s)",coches)
+
+database.commit() # Guarda los cambios en la base de datos que tenemos dentro del cursor
